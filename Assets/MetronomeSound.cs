5c4ed6a139fad4d48f864a99dc01cf40c78f61c1
Vsevolod <onewhosaynope@gmail.com>
2020-02-12 16:56:51 +0300

    hitboxes

---
+++ Assets/MetronomeSound.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetronomeSound : MonoBehaviour {
+
+    public AudioSource sound;
+
+    void OnTriggerEnter(Collider collider) {
+        sound.Play();
+    }
+}
