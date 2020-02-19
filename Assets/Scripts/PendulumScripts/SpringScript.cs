e02c99505f57a66cd78abc78b4da648cbda70d5c
onewhosaynope <onewhosaynope@gmail.com>
2020-02-19 18:00:58 +0300

    hinge joint script update

--- Assets/Scripts/PendulumScripts/SpringScript.cs
+++ Assets/Scripts/PendulumScripts/SpringScript.cs
@@ -1,25 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class SpringScript : MonoBehaviour
 {
-    public GameObject pivot;
-    private Quaternion angle;
-    
+    private HingeJoint hinge;
     // Start is called before the first frame update
     void Start()
     {
-        angle = pivot.transform.rotation;
+        hinge = GetComponent<HingeJoint>();
+        float y = transform.rotation.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Z))
+        var rotY = transform.localRotation.eulerAngles.y;
+        rotY = rotY >= 180 ? rotY - 360 : rotY;
+
+        if (rotY > 15)
         {
-            angle.y =- 5;
-            pivot.transform.rotation = angle;
+            Debug.Log($"More than 15 \nCurrent angle: {rotY}");
+            var spring = hinge.spring;
+            spring.targetPosition = -10;
+            hinge.spring = spring;
+        }
+        else if (rotY < -15)
+        {
+            Debug.Log($"Less than -15 \nCurrent angle: {rotY}");
+            var spring = hinge.spring;
+            spring.targetPosition = 10;
+            hinge.spring = spring;
         }
     }
 }
