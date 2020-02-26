85e19eeaead86b10f69870be36ba18a023ea6541
Seva <onewhosaynope@gmail.com>
2020-02-26 13:15:56 +0300

    pivot forces without time

--- Assets/Scripts/PendulumScripts/PivotForces1.cs
+++ Assets/Scripts/PendulumScripts/PivotForces1.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 // run at collisionExit
 // pause at collisionEnter
 
-public class PivotForces : MonoBehaviour
+public class PivotForces1 : MonoBehaviour
 {
     public float amplitude = 20f;
     public float period = 2f;
     private bool collided = false;
+    public float step = 0.01f;
+    private float timer;
     
     // Start is called before the first frame update
     void Start()
     {
-        Time timer_start;
+        float timer = 0.5f;
     }
 
     // Update is called once per frame
@@ -23,9 +25,10 @@ public class PivotForces : MonoBehaviour
     {
         if (collided)
         {
-            float angle2 = amplitude * Mathf.Cos(2 * Mathf.PI * Time.time / period);
+            timer = timer + step;
+            float angle2 = amplitude * Mathf.Cos(2 * Mathf.PI * timer / period);
             transform.localRotation = Quaternion.Euler(0f, angle2, 0f);
-            Debug.Log(Time.time);
+            Debug.Log(timer);
         }
 
         if (Input.GetKey(KeyCode.Z))
