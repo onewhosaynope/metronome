4d5a2b33515f37497d43d0dd017a84a416392887
Seva <onewhosaynope@gmail.com>
2020-02-27 01:32:41 +0300

    collider solution

---
+++ Assets/Scripts/PendulumScripts/ColliderRight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderRight : MonoBehaviour
+{
+    public GameObject pivot;
+    private PivotForces1 pivotValues;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivotValues = pivot.GetComponent<PivotForces1>();
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision other)
+    {
+        pivotValues.rotate = false;
+        Debug.Log("Right collider Enter");
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        pivotValues.step = -0.01f;
+        pivotValues.rotate = true;
+        Debug.Log("Right collider Exit");
+    }
+}
--- Assets/Scripts/PendulumScripts/PivotForces1.cs
+++ Assets/Scripts/PendulumScripts/PivotForces1.cs
@@ -10,51 +10,47 @@ public class PivotForces1 : MonoBehaviour
 {
     public float amplitude = 20f;
     public float period = 2f;
-    private bool collided = false;
-    public float step = 0.01f;
+    public bool rotate = false;
+    public float step;
     private float timer;
     
     // Start is called before the first frame update
     void Start()
     {
-        float timer = 0.5f;
+        timer = 0.5f;
+        step = 0.01f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (collided)
+        if (rotate)
         {
             timer = timer + step;
             float angle2 = amplitude * Mathf.Cos(2 * Mathf.PI * timer / period);
             transform.localRotation = Quaternion.Euler(0f, angle2, 0f);
             Debug.Log(timer);
         }
-
-        if (Input.GetKey(KeyCode.Z))
-        {
-            collided = false;
-            
-        } else if (Input.GetKey(KeyCode.X))
-        {
-            Debug.Log(transform.rotation.y);
-            Debug.Log(transform.eulerAngles.y);
-            collided = true;
-        }
     }
 
 
-    /*
-    private void OnCollisionEnter(Collision other)
+    /*private void OnCollisionEnter(Collision other)
     {
-        collided = false;
+        rotate = false;
+        Debug.Log(other.gameObject.name);
     }
 
     private void OnCollisionExit(Collision other)
     {
+        if (other.gameObject.tag == "left")
+        {
+            step = -0.01f;
+        } else if (other.gameObject.tag == "right")
+        {
+            step = 0.01f;
+        }
         Debug.Log(transform.rotation.y);
         Debug.Log(transform.eulerAngles.y);
-        collided = true;
-    } 
-    */
+        rotate = true;
+    } */
 }
